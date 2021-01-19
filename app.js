709b63f297f7b09d29087347c4e1814ba3e291b3
Hankscr459 <defcw58@gmail.com>
2021-01-19 13:25:59 +0800

    Arrays Types

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name : string
+//     age : number
+// } = {
 var person = {
     name: 'Hank',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivitiew;
+favoriteActivitiew = ['Sports'];
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
-const person: {
-    name : string
-    age : number
-} = {
+// const person: {
+//     name : string
+//     age : number
+// } = {
+const person = {
     name: 'Hank',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivitiew: string[]
+favoriteActivitiew = ['Sports']
+
 console.log(person)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
