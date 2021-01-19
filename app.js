092e454831e2e4ea3c1b73fc3d12834f75d7e84f
Hankscr459 <defcw58@gmail.com>
2021-01-19 14:06:56 +0800

    Working with Enums

--- app.js
+++ app.js
@@ -2,17 +2,40 @@
 //     name : string
 //     age : number
 // } = {
+// const person : {
+//     name: string
+//     age: number
+//     hobbies: string[]
+//     role: [number, string]
+// } = {
+//     name: 'Hank',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 200] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Hank',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
-person.role.push(0, 'admin');
-var favoriteActivitiew;
-favoriteActivitiew = ['Sports'];
-console.log(person);
+// person.role.push(0, 'admin')
+// let favoriteActivitiew: string[]
+// favoriteActivitiew = ['Sports']
+console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is Author');
 }
