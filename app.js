fdeac40ca7dd179fb1bc4d3f9c202da0348d2206
Hankscr459 <defcw58@gmail.com>
2021-01-19 13:15:38 +0800

    Object Types

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-function add(n1, n2, showResult, pharse) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw new Error('Incorrect input!')
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(pharse + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var PrintResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, PrintResult, resultPhrase);
+var person = {
+    name: 'Hank',
+    age: 30
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,19 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, pharse: string) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw new Error('Incorrect input!')
-    // }
-    const result = n1 + n2
-    if (showResult) {
-        console.log(pharse + result)
-    } else {
-        return n1 + n2
-    }
+const person: {
+    name : string
+    age : number
+} = {
+    name: 'Hank',
+    age: 30
 }
 
-let number1 : number
-number1 = 5
-const number2 = 2.8
-const PrintResult = true
-const resultPhrase = 'Result is: '
-
-add(number1, number2, PrintResult, resultPhrase)
+console.log(person)
