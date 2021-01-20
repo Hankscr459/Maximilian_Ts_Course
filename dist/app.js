5a4d5eedf346c37d596326241b9f5ce0c9706f0a
Hankscr459 <defcw58@gmail.com>
2021-01-20 16:05:58 +0800

    "private" and "public" Access Modifiers

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,23 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Maru');
+accounting.addEmployee('LuLu');
 accounting.describe();
-const accountingCopy = { name: 's', describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string
+    public name: string
+    private employees: string[] = []
 
     constructor(n: string) {
         this.name = n
@@ -8,12 +9,29 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name)
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
 const accounting = new Department('Accounting')
 
+accounting.addEmployee('Maru')
+accounting.addEmployee('LuLu')
+
+// accounting.employees[2] = 'DD'
+
+accounting.name = 'Andrew'
+
 accounting.describe()
+accounting.printEmployeeInformation()
 
-const accountingCopy = { name: 's', describe: accounting.describe }
+// const accountingCopy = { name: 's', describe: accounting.describe }
 
-accountingCopy.describe()
+// accountingCopy.describe()
