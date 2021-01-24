f086b0a3a14ad7c0ac45d224099287332fc891e0
Hankscr459 <defcw58@gmail.com>
2021-01-24 13:34:41 +0800

    Working with Constraints

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,13 @@
 //     // data.split(' ')
 // })
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB)
 }
 
-const mergeObj = merge<{name: string, hobbies: string[]}, {age: number}>({ name: 'Hank', hobbies: ['Sport'] }, { age: 27 })
+// const mergeObj = merge<{name: string, hobbies: string[]}, {age: number}>({ name: 'Hank', hobbies: ['Sport'] }, { age: 27 })
+const mergeObj = merge({ name: 'Hank', hobbies: ['Sport'] }, { age: 27 })
+
 console.log(mergeObj)
 
 // console.log(merge({name: 'Hank'}, {age: 27}))
