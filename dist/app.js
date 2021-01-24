ad1a6363c585676cb9b44eee793bf53a3db80349
Hankscr459 <defcw58@gmail.com>
2021-01-24 11:56:41 +0800

    Creating a Generic Function

--- dist/app.js
+++ dist/app.js
@@ -1,62 +1,7 @@
 "use strict";
-const e1 = {
-    name: 'Hank',
-    privileges: ['create-server'],
-    startDate: new Date()
-};
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
-function printEmployeeInformation(emp) {
-    console.log('Name: ' + emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log('Privileges: ' + emp.startDate);
-    }
-}
-printEmployeeInformation(e1);
-class Car {
-    drive() {
-        console.log('Driving...');
-    }
-}
-class Trunk {
-    drive() {
-        console.log('Driving a trunk');
-    }
-    loadCargo(amount) {
-        console.log('Loading cargo ...' + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Trunk();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Trunk) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-    }
-    console.log('Moving at speed: ' + speed);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 10 });
-const userInputElement = document.getElementById('user-input');
-if (userInputElement) {
-    userInputElement.value = 'Hi there!';
-}
+const mergeObj = merge({ name: 'Hank' }, { age: 27 });
+console.log(mergeObj);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-const names: Array<string> = []
+// const names: Array<string> = []
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(7)
-    }, 2000)
-})
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(7)
+//     }, 2000)
+// })
 
-promise.then(data => {
-    // data.split(' ')
-})
+// promise.then(data => {
+//     // data.split(' ')
+// })
+
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+const mergeObj = merge<{name: string, hobbies: string[]}, {age: number}>({ name: 'Hank', hobbies: ['Sport'] }, { age: 27 })
+console.log(mergeObj)
+
+// console.log(merge({name: 'Hank'}, {age: 27}))
